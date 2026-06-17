fc5139f232e0923870c519b6f0f3898bc9c32a09
eririii2 <leisrylle@gmail.com>
2026-06-17 18:00:25 +0800

    updated

--- BSIT31E1_PRELIM_A2_RAMONESLEISBETHANN/Program.cs
+++ BSIT31E1_PRELIM_A2_RAMONESLEISBETHANN/Program.cs
@@ -1,1 +1,51 @@
-﻿class 
+﻿while (true)
+{
+    Console.WriteLine("Enter number ( exit to quit )");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "exit")
+    {
+        break;
+    }
+
+    Console.WriteLine("Enter number ( exit to quit )");
+    string input2 = Console.ReadLine();
+
+    if (input2.ToLower() == "exit")
+    {
+        break;
+    }
+
+    Console.WriteLine("Enter Operator: +, -, *, /");
+    string selection = Console.ReadLine();
+    int a = int.Parse(input);
+    int b = int.Parse(input2);
+
+    if (selection == "+")
+    {
+        int total = a + b;
+        Console.WriteLine(total);
+    }
+
+    else if (selection == "-")
+    {
+        int total = a - b;
+        Console.WriteLine(total);
+    }
+
+    else if (selection == "*")
+    {
+        int total = a * b;
+        Console.WriteLine(total);
+    }
+    else if (selection == "/")
+    {
+        int total = a / b;
+        Console.WriteLine(total);
+    }
+
+    else
+    {
+        Console.WriteLine("Invalid");
+    }
+}
